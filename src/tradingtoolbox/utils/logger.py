43149ecddd4ce9cd8f4cdb4d7fee431689226f2a
Piotr Yordanov <piotr@piotryordanov.com>
2025-04-29 21:04:05 +0200

    fix: depth

--- src/tradingtoolbox/utils/logger.py
+++ src/tradingtoolbox/utils/logger.py
@@ -186,7 +186,8 @@ class Logger:
             *obj: Variable number of objects to log (can be any type including tuple)
         """
         for item in obj:
-            self.logger.opt(depth=2).warning(pretty_repr(item))
+            self.logger.opt(depth=1).warning(pretty_repr(item))
+            rprint(item)
 
     def info(self, *obj):
         """
